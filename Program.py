68522a7ae573db790c9a9d00adde25704d739351
Nick Pretious <129784159+Npretious@users.noreply.github.com>
2023-11-16 13:58:02 -0700

    Initial commit

---
+++ Program.py
@@ -0,0 +1,12 @@
+﻿#Email Slicer Program
+
+#Fetches users email address
+email = input("What is your email address?: ").strip()
+#Slices out username
+user_name = email[:email.index("@")]
+#Slices out domain name
+domain_name = email[email.index("@")+1:]
+#Format Message
+output = "Your username is '{}' and your domain name is '{}'".format(user_name,domain_name)
+#Prints output to display
+print(output)
